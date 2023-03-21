70f6a0fb0b1b8698334f4e548602c929d564268b
+ <+>
2023-03-21 22:03:06 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const names = ["Guadalupe", "Ollie", "Aki"]
 
 function writeCards(names, messages){
-    for (let i = 0; i < messages.length; i++) {
-    console.log(`Thank you ${names[i]} for the wonderfl suprise gifts!`);
-    debugger;
+    let newArr = []
+    for (let i = 0; i < names.length; i++) {
+    return newArr.map(`Thank you ${names[i]} for the wonderfl suprise gifts!`);
   }
 
 }
